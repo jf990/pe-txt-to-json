95cf6e131eb79c50ecefa1ad8a547a7dedb556eb
John Foster <jfoster@esri.com>
2017-09-12 09:58:33 -0700

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # pe-txt-to-json
-Convert Esri PE text files to minified JSON format
+Convert Esri PE text files to minified JSON format using node.js.
+
+This initial version is a bit inflexible. Here's how it works:
+
+ * Receive 4 TXT files from the Esri PE Team:
+    + Geographic Transforms
+    + Geographic Coordinate Systems
+    + Vertical Transforms
+    + Vertical Coordinate Systems
+
+ * Put those TXT files in the data folder.
+ * Rename those files to the 4 file names used by this conversion process:
+    + gcs_wkt_areas.txt
+    + gt_wkt_areas.txt
+    + vcs_wkt_areas.txt
+    + vt_wkt_areas.txt
+
+ * Only if required (because something changed), open `index.js` and update the `fileList` array with:
+    + file name
+    + file description
+    + column order of the tab delimited columns and the matching JSON key name to use to represent that column. Note the code relies on these specific column names so if they change the code consuming this data will also have to change to match.
+
+ * Be sure to run `npm install`
+
+ * To run the conversion, `npm run`
+
+The converted files are placed in teh `data` folder with the same file name and a `.min.json` extension.
+
---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+ * Entry point for the Projection Engine JSON data file transformer. The goal of this project is
+ * to optimize the TXT files we receive from PE for use on a website.
+ *
+ * We receive TXT files from PE team. These files are tab separated files and the columns are slightly different. This
+ * process will read each TXT file, convert it to a JSON representation and save a minified version of it.
+ */
+
+const csvtojson = require('csvtojson');
+const jsonfile = require('jsonfile');
+const rootPath = 'data/';
+var fileList = [ // define the list of files to convert, the column order and name to use for that column
+    {filename: "gcs_wkt_areas", name:"Geographic Coordinates", columns: ["WKID", "Name", "WKT", "AreaOfUse", "MinimumLatitude", "MinimumLongitude", "MaximumLatitude", "MaximumLongitude"]},
+    {filename: "gt_wkt_areas",  name:"Geographic Transforms",  columns: ["WKID", "Name", "WKT", "Accuracy", "AreaOfUse", "MinimumLatitude", "MinimumLongitude", "MaximumLatitude", "MaximumLongitude"]},
+    {filename: "vcs_wkt_areas", name:"Vertical Coordinates",   columns: ["WKID", "Name", "WKT", "AreaOfUse", "MinimumLatitude", "MinimumLongitude", "MaximumLatitude", "MaximumLongitude"]},
+    {filename: "vt_wkt_areas",  name:"Vertical Transforms",    columns: ["WKID", "Name", "WKT", "Accuracy", "AreaOfUse", "MinimumLatitude", "MinimumLongitude", "MaximumLatitude", "MaximumLongitude"]}
+];
+
+
+var convertFiles = function() {
+
+    fileList.forEach(function(fileParameters) {
+        var filePath = rootPath + fileParameters.filename + '.txt';
+        var transformedProjectionObject = [];
+        csvtojson({
+            noheader: false,
+            trim: true,
+            delimiter: ["\t"],
+            headers: fileParameters.columns
+            })
+            .fromFile(filePath)
+            .on('json', function (projectionObject) {
+                transformedProjectionObject[transformedProjectionObject.length] = {
+                    wkid: projectionObject.WKID,
+                    name: projectionObject.Name,
+                    wkt: projectionObject.WKT,
+                    areaofuse: projectionObject.AreaOfUse,
+                    extent: [projectionObject.MinimumLatitude, projectionObject.MinimumLongitude, projectionObject.MaximumLatitude, projectionObject.MaximumLongitude],
+                    accuracy: (typeof projectionObject.Accuracy !== 'undefined' ? projectionObject.Accuracy : "0")
+                };
+            })
+            .on('done', function (readError) {
+                if (typeof readError !== 'undefined' && readError !== null) {
+                    console.log("Error reading TXT file " + filePath + ": " + readError.message);
+                } else {
+                    var writeFilePath = rootPath + fileParameters.filename + '.min.json';
+                    jsonfile.writeFile(writeFilePath, transformedProjectionObject, {spaces: 0}, function (writeError) {
+                        if (typeof writeError !== 'undefined' && writeError !== null) {
+                            console.log("Error writing " + filePath + ": " + writeError.message);
+                        }
+                    });
+                }
+                console.log('completed ' + filePath);
+            });
+    });
+};
+
+exports.process = convertFiles;
+
+exports.fileList = function() {
+    return fileList;
+};
+
+convertFiles();
